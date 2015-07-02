585fcba0d78191abb7e2bc39d5f6ddf1721c0129
Herbert G. Fischer <herbert.fischer@gmail.com>
2015-07-02 08:39:15 -0300

    WIP

--- static/js/wpm-0.0.1.js
+++ static/js/wpm-0.0.1.js
@@ -9,27 +9,39 @@ function addEvent(element, eventName, fn) {
 addEvent(window, 'load', function() {
     var http = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
     http.onreadystatechange = function() {
-        if (http.readyState == 4) {
-            console.log(http.status);
-            console.log(http.responseText);
-        }
+        //if (http.readyState == 4) {
+        //    console.log(http.status);
+        //    console.log(http.responseText);
+        //}
     }
 
     http.open('POST', 'http://httpbin.org/post', true);
     http.setRequestHeader('Content-Type', 'application/json');
     var wp = window.performance
 
-    var perf = { 'nav': {}, 'mem': {}, 'tim': {}, 'entries': [] };
-    for (var k in wp.navigation) perf['nav'][k] = wp.navigation[k];
-    for (var k in wp.memory) perf['mem'][k] = wp.memory[k];
-    for (var k in wp.timing) perf['tim'][k] = wp.timing[k];
-    for (var e in wp.getEntries()) {
-        for (var k in e) {
-            perf['entries'][k] = wp.getEntries();
+    var perf = { 
+        'navigation': {}, 
+        'memory': {}, 
+        'timing': {}, 
+        'entries': [], 
+        'navigator': {} 
+    };
+
+    for (var k in window.navigator) perf['navigator'][k] = window.navigator[k];
+    for (var k in wp.navigation) perf['navigation'][k] = wp.navigation[k];
+    for (var k in wp.memory) perf['memory'][k] = wp.memory[k];
+    for (var k in wp.timing) perf['timing'][k] = wp.timing[k];
+
+    var entries = wp.getEntries();
+    for (var i in entries) {
+        var entry = {};
+        for (var j in entries[i]) {
+            entry[j] = entries[i][j];
         }
+        perf['entries'][i] = entry;
     }
 
-    console.log(perf);
-
-    http.send(JSON.stringify(perf));
+    var json = JSON.stringify(perf, null, "\t");
+    console.log(json);
+    http.send(json);
 });
